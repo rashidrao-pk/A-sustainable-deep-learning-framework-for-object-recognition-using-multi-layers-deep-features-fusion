5c3bcfe1051bfabdd2fbc6e27c3681919e679865
Muhammad Rashid <rashid047571@gmail.com>
2022-07-06 23:14:38 +0500

    Delete Find_Entropy.m

--- Find_Entropy.m
+++
@@ -1,7 +0,0 @@
-function vecFinal= Find_Entropy(score,iterations)
- for i = 1 : iterations
-      entroVec(i) = entropy(score(:,i));
-  end
-  [entVals, entIndx] = sort(entroVec,'descend');
-  vecFinal = score(:,entIndx);
-end
